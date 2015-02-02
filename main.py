d6a20a1de0a6afd70232acbe22301a41afea9eb7
Sebastien Rousseau <sebastien.rousseau@reedia.com>
2015-02-02 16:43:03 +0000

    Various updates (Version 1.0)
    Signed-off-by: Sebastien Rousseau <sebastien.rousseau@reedia.com>

--- main.py
+++ main.py
@@ -7,51 +7,56 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        title = "Welcome"
-        description = ""
-        keywords = ""
+        title = "White Collar Boxing Training"
+        subtitle = "World-class boxing training London"
+        description = "White Collar Boxing Training offers premier world class training for professionals throughout London and the UK."
+        keywords = "boxing training, white collar, collar boxing, advanced training,london,uk,training sessions,professionals,trainers"
         banner="<video style=\"width:100%;max-width: 100%;height:auto;z-index:-100;display: block;\" autoplay poster=\"/images/other/gary.png\"><source src=\"/videos/gary.ogv\" type=\"video/ogg\"><source src=\"/videos/gary.mp4\" type=\"video/mp4\"></video>"
-        template_vars = {"title":title,"description":description,"keywords": keywords,"banner": banner}
+        template_vars = {"title":title,"subtitle":subtitle,"description":description,"keywords": keywords,"banner": banner}
         template = JINJA_ENVIRONMENT.get_template("index.html")
         self.response.write(template.render(template_vars))
 
 class StoryHandler(webapp2.RequestHandler):
     def get(self):
         title = "Our Story"
-        description = "White Collar Boxing Training offers premier world class training for professionals throughout London and the UK."
-        keywords = "white collar,collar boxing,boxing training,london,uk,training sessions,professionals,trainers,big fight"
+        subtitle = "Premier world-class boxing training in London"
+        description = "We help professionals suit up and properly prepare for their big fight regardless of their current ability or skill set."
+        keywords = "white collar,collar boxing,boxing training,training methods,boxer,big fight,training sessions,advanced boxing,advanced training"
         banner = "<picture><source srcset=\"/images/banner/banner.svg\" type=\"image/svg+xml\"><img src=\"/images/banner/banner.jpg\" width=\"960\" height=\"378\" alt=\"White Collar Boxing Training\" /></picture>"
-        template_vars = {"title":title,"description":description,"keywords": keywords,"banner": banner}
+        template_vars = {"title":title,"subtitle":subtitle,"description":description,"keywords": keywords,"banner": banner}
         template = JINJA_ENVIRONMENT.get_template("story.html")
         self.response.write(template.render(template_vars))
 
 class TrainersHandler(webapp2.RequestHandler):
     def get(self):
-        title = "Our Trainers"
-        description = ""
-        keywords = ""
+        title = "Trainers"
+        subtitle = "We take pride in pushing our clients to their limits"
+        description = "Over 20 years of experience with white collar boxing in London"
+        keywords = "trainers,white collar,collar boxing,boxing training,gary foley,sean murphy,boxing training,boxer winning,our training"
         banner = "<picture><source srcset=\"/images/banner/banner_3.svg\" type=\"image/svg+xml\"><img src=\"/images/banner/banner_3.jpg\" width=\"960\" height=\"378\" alt=\"White Collar Boxing Training\" /></picture>"
-        template_vars = {"title":title,"description":description,"keywords": keywords,"banner": banner}
+        template_vars = {"title":title,"subtitle":subtitle,"description":description,"keywords": keywords,"banner": banner}
         template = JINJA_ENVIRONMENT.get_template("trainers.html")
         self.response.write(template.render(template_vars))
 
 class WhatsInvolvedHandler(webapp2.RequestHandler):
     def get(self):
-        title = "What's involved"
-        description = ""
-        keywords = ""
+        title = "Training"
+        subtitle = "Offering a variety of boxing training levels"
+        description = "Our trainers will help you understand the basics of white-collar boxing and guide you through each step one-on-one until you master each skill"
+        keywords = "collar boxing,white collar,boxing training,training levels,skill set,intense training,london uk,beginner training,training advanced"
         banner = "<picture><source srcset=\"/images/banner/banner_2.svg\" type=\"image/svg+xml\"><img src=\"/images/banner/banner_2.jpg\" width=\"960\" height=\"378\" alt=\"White Collar Boxing Training\" /></picture>"
-        template_vars = {"title":title,"description":description,"keywords": keywords,"banner": banner}
+        template_vars = {"title":title,"subtitle":subtitle,"description":description,"keywords": keywords,"banner": banner}
         template = JINJA_ENVIRONMENT.get_template("whats-involved.html")
         self.response.write(template.render(template_vars))
 
 class GetInTouchHandler(webapp2.RequestHandler):
     def get(self):
         title = "Get in touch"
-        description = ""
-        keywords = ""
+        subtitle = "Learn from the best white collar boxing trainers"
+        description = "Our white collar boxing training is specifically tailored to help average business professionals unlock their physical and psychological strength."
+        keywords = "collar boxing,white collar,our training,boxing trainers,training sessions,white collar boxing,get in touch,contact us today,boxing trainers"
         banner = "<picture><source srcset=\"/images/banner/banner_4.svg\" type=\"image/svg+xml\"><img src=\"/images/banner/banner_4.jpg\" width=\"960\" height=\"378\" alt=\"White Collar Boxing Training\" /></picture>"
-        template_vars = {"title":title,"description":description,"keywords": keywords,"banner": banner}
+        template_vars = {"title":title,"subtitle":subtitle,"description":description,"keywords": keywords,"banner": banner}
         template = JINJA_ENVIRONMENT.get_template("get-in-touch.html")
         self.response.write(template.render(template_vars))
 
